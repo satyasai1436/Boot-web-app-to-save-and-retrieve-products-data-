e8a2f6bf792ebb3405a0f5f88bbc0b4825b425b1
satyasai1436 <saisatyateja1436@gmail.com>
2024-03-21 18:29:26 +0530

    first message

--- src/main/java/in/satya/Controller/productController.java
+++ src/main/java/in/satya/Controller/productController.java
@@ -4,8 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import in.satya.Entity.Product;
@@ -17,6 +21,8 @@ public class productController {
 	@Autowired
 	private productService pService;
 
+	// to load the page and showing the empty form page and we need to set data
+
 	@GetMapping("/")
 	public ModelAndView loadForm() {
 		ModelAndView mav = new ModelAndView();
@@ -25,21 +31,53 @@ public class productController {
 		return mav;
 	}
 
+	// after setting the data we need to do some action
+
 	@PostMapping("/form")
-	public ModelAndView setAll(Product p) {
-		ModelAndView mav = new ModelAndView();
-		pService.setData(p);
-		mav.addObject("msg", "success");
-		mav.setViewName("data1");
-		return mav;
+	public String setAll(@ModelAttribute("set") Product p, Model m) {
+		Product Data = pService.setData(p);
+		System.out.println("Inside Save" + Data);
+		if (Data.getpId() != null)
+			m.addAttribute("msg", "Success");
+		else
+			m.addAttribute("msg", "Failed");
+
+		return "Set";
 	}
+
+	// to retrieving the data
+
 	@GetMapping("/get1")
 	public ModelAndView getAll() {
 		ModelAndView mav = new ModelAndView();
 		List<Product> list = pService.getAll();
-		mav.addObject("msg1",list );
+		mav.addObject("msg1", list);
 		mav.setViewName("data");
 		return mav;
+	}
+
+	// to deleting the data using pId
+	@GetMapping("/get2")
+	public ModelAndView delete(@RequestParam("pId") Integer pId) {
+		ModelAndView mav = new ModelAndView();
+		boolean status = pService.delete(pId);
+		if (status) {
+			mav.addObject("del", "Success");
+		} else {
+			mav.addObject("del", "Failed");
+		}
+		mav.setViewName("data");
+		return mav;
+	}
+
+	@GetMapping("/edit")
+	public String showUpadateForm(Model model, Integer pId) {
+		Product updatedObj = pService.update(pId);
+
+		//System.out.println("Inside edit" + updatedObj);
+		model.addAttribute("set", updatedObj);
+		pService.delete(pId);
+		return "Set";
 	}
 
 }
--- src/main/java/in/satya/Service/productService.java
+++ src/main/java/in/satya/Service/productService.java
@@ -1,6 +1,7 @@
 package in.satya.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -14,16 +15,36 @@ public class productService {
 	private productRepo Repo;
 
 	public Product setData(Product p) {
-		
+
 		return Repo.save(p);
 
 	}
 
 	public List<Product> getAll() {
 		List<Product> list = Repo.findAll();
-			
+
 		return list;
 
+	}
+
+	public boolean delete(Integer pId) {
+		boolean flag = false;
+		try {
+			Repo.deleteById(pId);
+			flag = true;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return flag;
+	}
+	public Product update(Integer pId) {
+		Optional<Product> id = Repo.findById(pId);
+		if(id.isPresent()) {
+			return id.get();
+		}
+		else {
+			return null;
+		}
 	}
 
 }
